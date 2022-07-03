0dc4e195de2a999ad12fd059c23d95d16a1e2b61
bnGil <gilbn1994@gmail.com>
2022-07-03 16:46:45 +0300

    added initial files

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ import cors from "cors";
 import { fileURLToPath } from "url";
 
 import "./db/mongoose.js";
-import { router as v1 } from "./routes/v1.routes.js";
 
 const fileName = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(fileName);
@@ -16,15 +15,6 @@ app.use(express.static(publicPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-app.get("/test", (req, res) => {
-  try {
-    res.send({ __dirname, publicPath });
-  } catch (e) {
-    res.send(e.message);
-  }
-});
-app.use("/api/v1", v1);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
