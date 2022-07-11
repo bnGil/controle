e0bbfaeff0a2abb93eedfa5df7aa555b7911d7d3
bnGil <gilbn1994@gmail.com>
2022-07-11 12:32:06 +0300

    added try/catch to the scrapers

--- server/scraper/fiverr.scraper.js
+++ server/scraper/fiverr.scraper.js
@@ -5,55 +5,61 @@ export async function scrapeFiverr() {
   puppeteer.use(StealthPlugin());
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.fiverr.com/jobs/teams?location=tlv", {
-    waitUntil: "networkidle0",
-  });
+  try {
+    await page.goto("https://www.fiverr.com/jobs/teams?location=tlv", {
+      waitUntil: "networkidle0",
+    });
 
-  const deps = await page.$$eval("div.vlbNOzf", (departments) =>
-    departments.map((department) => {
-      const departmentName = department.querySelector("a > h2").textContent;
-      const company = "Fiverr";
-      const location = "Tel Aviv";
-      const depJobs = Array.from(department.querySelectorAll("div a.OuRgckW"));
-      return depJobs.map((job) => ({
-        jobPageLink: job.href,
-        title: job.querySelector("div > h6").textContent,
-        company,
-        location,
-        departmentName,
-      }));
-    })
-  );
-  const jobs = deps.flat();
-
-  for (let i = 0; i < jobs.length; i++) {
-    await Promise.all([
-      page.waitForNavigation(),
-      page.goto(jobs[i].jobPageLink),
-    ]);
-
-    const description = await page.$$eval(
-      "div.page-container > div.details-section > div:nth-child(1) > div > p",
-      (ps) => ps.map((p) => p.textContent)
+    const deps = await page.$$eval("div.vlbNOzf", (departments) =>
+      departments.map((department) => {
+        const departmentName = department.querySelector("a > h2").textContent;
+        const company = "Fiverr";
+        const location = "Tel Aviv";
+        const depJobs = Array.from(
+          department.querySelectorAll("div a.OuRgckW")
+        );
+        return depJobs.map((job) => ({
+          jobPageLink: job.href,
+          title: job.querySelector("div > h6").textContent,
+          company,
+          location,
+          departmentName,
+        }));
+      })
     );
+    const jobs = deps.flat();
 
-    const responsibilities = await page.$$eval(
-      "div.page-container > div.details-section > div:nth-child(2) > div > ul > li",
-      (lis) => lis.map((li) => li.textContent)
-    );
+    for (let i = 0; i < jobs.length; i++) {
+      await Promise.all([
+        page.waitForNavigation(),
+        page.goto(jobs[i].jobPageLink),
+      ]);
 
-    const requirements = await page.$$eval(
-      "div.page-container > div.details-section > div:nth-child(3) > div > ul > li",
-      (lis) => lis.map((li) => li.textContent)
-    );
+      const description = await page.$$eval(
+        "div.page-container > div.details-section > div:nth-child(1) > div > p",
+        (ps) => ps.map((p) => p.textContent)
+      );
 
-    jobs[i].jobDescription = {
-      description,
-      responsibilities,
-      requirements,
-    };
+      const responsibilities = await page.$$eval(
+        "div.page-container > div.details-section > div:nth-child(2) > div > ul > li",
+        (lis) => lis.map((li) => li.textContent)
+      );
 
-    jobs[i].jobId = getFiverrJobIdFromURL(jobs[i].jobPageLink);
+      const requirements = await page.$$eval(
+        "div.page-container > div.details-section > div:nth-child(3) > div > ul > li",
+        (lis) => lis.map((li) => li.textContent)
+      );
+
+      jobs[i].jobDescription = {
+        description,
+        responsibilities,
+        requirements,
+      };
+
+      jobs[i].jobId = getFiverrJobIdFromURL(jobs[i].jobPageLink);
+    }
+  } catch (err) {
+    console.log(err.message);
   }
 
   browser.close();
--- server/scraper/naturalInt.scraper.js
+++ server/scraper/naturalInt.scraper.js
@@ -5,58 +5,62 @@ import { pascalCaseStr } from "../utils/utilFunctions.js";
 export async function scrapeNaturalInt() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.naturalint.com/jobs/");
+  try {
+    await page.goto("https://www.naturalint.com/jobs/");
 
-  const jobs = await page.$$eval("div.job-items a", (jobItems) =>
-    jobItems.map((jobItem) => {
-      const jobPageLink = jobItem.href;
-      const title = jobItem.querySelector("h2.item-title").textContent;
-      const department = jobItem.querySelector("h3.item-cat").textContent;
-      const location = "Tel Aviv";
-      const company = "Natural Intelligence";
-      return {
-        title,
-        department,
-        jobPageLink,
-        location,
-        company,
+    const jobs = await page.$$eval("div.job-items a", (jobItems) =>
+      jobItems.map((jobItem) => {
+        const jobPageLink = jobItem.href;
+        const title = jobItem.querySelector("h2.item-title").textContent;
+        const department = jobItem.querySelector("h3.item-cat").textContent;
+        const location = "Tel Aviv";
+        const company = "Natural Intelligence";
+        return {
+          title,
+          department,
+          jobPageLink,
+          location,
+          company,
+        };
+      })
+    );
+
+    for (let i = 0; i < jobs.length; i++) {
+      await Promise.all([
+        page.waitForNavigation(),
+        page.goto(jobs[i].jobPageLink),
+        page.waitForSelector(".jobs-position-info"),
+      ]);
+
+      const description = await page.$$eval(
+        "div.jobs-position-section.comeet-position-description p",
+        (ps) => ps.map((p) => p.textContent)
+      );
+      description.pop();
+
+      const responsibilities = await page.$$eval(
+        "div.jobs-position-section.comeet-position-description > ul li",
+        (lis) => lis.map((li) => li.textContent)
+      );
+
+      const requirements = await page.$$eval(
+        "div.jobs-position-section.comeet-position-requirements > ul li",
+        (lis) => lis.map((li) => li.textContent)
+      );
+
+      jobs[i].department = pascalCaseStr(jobs[i].department);
+      jobs[i].title = pascalCaseStr(jobs[i].title);
+
+      jobs[i].jobDescription = {
+        description,
+        responsibilities,
+        requirements,
       };
-    })
-  );
 
-  for (let i = 0; i < jobs.length; i++) {
-    await Promise.all([
-      page.waitForNavigation(),
-      page.goto(jobs[i].jobPageLink),
-      page.waitForSelector(".jobs-position-info"),
-    ]);
-
-    const description = await page.$$eval(
-      "div.jobs-position-section.comeet-position-description p",
-      (ps) => ps.map((p) => p.textContent)
-    );
-    description.pop();
-
-    const responsibilities = await page.$$eval(
-      "div.jobs-position-section.comeet-position-description > ul li",
-      (lis) => lis.map((li) => li.textContent)
-    );
-
-    const requirements = await page.$$eval(
-      "div.jobs-position-section.comeet-position-requirements > ul li",
-      (lis) => lis.map((li) => li.textContent)
-    );
-
-    jobs[i].department = pascalCaseStr(jobs[i].department);
-    jobs[i].title = pascalCaseStr(jobs[i].title);
-
-    jobs[i].jobDescription = {
-      description,
-      responsibilities,
-      requirements,
-    };
-
-    jobs[i].jobId = getNaturalJobIdFromURL(jobs[i].jobPageLink);
+      jobs[i].jobId = getNaturalJobIdFromURL(jobs[i].jobPageLink);
+    }
+  } catch (err) {
+    console.log(err.message);
   }
 
   browser.close();
