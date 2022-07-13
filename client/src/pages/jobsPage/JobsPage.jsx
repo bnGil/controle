35f8869110f7686c58746e0df002fac4b6581b5d
bnGil <gilbn1994@gmail.com>
2022-07-13 19:18:13 +0300

    created jobs page and job component basic style

--- client/src/pages/jobsPage/JobsPage.jsx
+++ client/src/pages/jobsPage/JobsPage.jsx
@@ -1,8 +1,19 @@
 import React, { useState } from "react";
+import Job from "../../components/job/Job";
 import "./jobsPage.css";
 
 function JobsPage() {
-  return <h1>jobs</h1>;
+  return (
+    <div className="jobspage-container">
+      <div className="search">
+        <input type="text" />
+      </div>
+      <div className="jobs-container">
+        <Job />
+        <Job />
+      </div>
+    </div>
+  );
 }
 
 export default JobsPage;
