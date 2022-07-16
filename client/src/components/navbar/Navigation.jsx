a0e133476bc944a38ba5e0b2182f94fb68ea085c
bnGil <gilbn1994@gmail.com>
2022-07-16 21:08:01 +0300

    updated style of navbar

--- client/src/components/navbar/Navigation.jsx
+++ client/src/components/navbar/Navigation.jsx
@@ -2,6 +2,50 @@ import { NavLink } from "react-router-dom";
 
 import "./navigation.css";
 
+// function Navigation({ user, logout, token }) {
+//   return (
+//     <div className="navbar-container">
+//       <nav className="navbar">
+//         <NavLink className="logo" exact to="/">
+//           Cont<span>role</span>
+//         </NavLink>
+//         <div className="links-container">
+//           <NavLink className="navlink" exact to="/">
+//             Home
+//           </NavLink>
+//           <NavLink className="navlink" to="/about">
+//             About
+//           </NavLink>
+//           <NavLink className="navlink" to="/jobs">
+//             Jobs
+//           </NavLink>
+//           <NavLink className="navlink" to="/applications">
+//             My Applications
+//           </NavLink>
+//         </div>
+//         <div className="rslinks-container">
+//           {token ? (
+//             <a href="#" className="navlink" onClick={logout}>
+//               Logout
+//             </a>
+//           ) : (
+//             <>
+//               <NavLink className="navlink" exact to="/register">
+//                 Register
+//               </NavLink>
+//               <NavLink className="navlink" exact to="/login">
+//                 Login
+//               </NavLink>
+//             </>
+//           )}
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// export default Navigation;
+
 function Navigation({ user, logout, token }) {
   return (
     <div className="navbar-container">
@@ -10,30 +54,30 @@ function Navigation({ user, logout, token }) {
           Cont<span>role</span>
         </NavLink>
         <div className="links-container">
-          <NavLink className="navlink" exact to="/">
+          <NavLink className="menu-li" exact to="/">
             Home
           </NavLink>
-          <NavLink className="navlink" to="/about">
+          <NavLink className="menu-li" to="/about">
             About
           </NavLink>
-          <NavLink className="navlink" to="/jobs">
+          <NavLink className="menu-li" to="/jobs">
             Jobs
           </NavLink>
-          <NavLink className="navlink" to="/applications">
+          <NavLink className="menu-li" to="/applications">
             My Applications
           </NavLink>
         </div>
         <div className="rslinks-container">
           {token ? (
-            <a href="#" className="navlink" onClick={logout}>
+            <a href="#" className="menu-li" onClick={logout}>
               Logout
             </a>
           ) : (
             <>
-              <NavLink className="navlink" exact to="/register">
+              <NavLink className="menu-li" exact to="/register">
                 Register
               </NavLink>
-              <NavLink className="navlink" exact to="/login">
+              <NavLink className="menu-li" exact to="/login">
                 Login
               </NavLink>
             </>
