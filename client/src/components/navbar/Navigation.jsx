54d02a70e6f574bffcc6c1e8adf48a5d410c1c58
bnGil <gilbn1994@gmail.com>
2022-07-16 22:05:53 +0300

    deleted useless comments

--- client/src/components/navbar/Navigation.jsx
+++ client/src/components/navbar/Navigation.jsx
@@ -2,51 +2,7 @@ import { NavLink } from "react-router-dom";
 
 import "./navigation.css";
 
-// function Navigation({ user, logout, token }) {
-//   return (
-//     <div className="navbar-container">
-//       <nav className="navbar">
-//         <NavLink className="logo" exact to="/">
-//           Cont<span>role</span>
-//         </NavLink>
-//         <div className="links-container">
-//           <NavLink className="navlink" exact to="/">
-//             Home
-//           </NavLink>
-//           <NavLink className="navlink" to="/about">
-//             About
-//           </NavLink>
-//           <NavLink className="navlink" to="/jobs">
-//             Jobs
-//           </NavLink>
-//           <NavLink className="navlink" to="/applications">
-//             My Applications
-//           </NavLink>
-//         </div>
-//         <div className="rslinks-container">
-//           {token ? (
-//             <a href="#" className="navlink" onClick={logout}>
-//               Logout
-//             </a>
-//           ) : (
-//             <>
-//               <NavLink className="navlink" exact to="/register">
-//                 Register
-//               </NavLink>
-//               <NavLink className="navlink" exact to="/login">
-//                 Login
-//               </NavLink>
-//             </>
-//           )}
-//         </div>
-//       </nav>
-//     </div>
-//   );
-// }
-
-// export default Navigation;
-
-function Navigation({ user, logout, token }) {
+function Navigation({ logout, token }) {
   return (
     <div className="navbar-container">
       <nav className="navbar">
