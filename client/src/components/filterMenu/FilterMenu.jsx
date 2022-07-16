cdabaa0f97921c85b8ee2fc3e536f491d5b84842
bnGil <gilbn1994@gmail.com>
2022-07-16 17:33:31 +0300

    changed buttons to li's

--- client/src/components/filterMenu/FilterMenu.jsx
+++ client/src/components/filterMenu/FilterMenu.jsx
@@ -14,31 +14,29 @@ function FilterMenu({ setIsFilterOpen }) {
         <div className="category-container">
           <h5 className="options-header">Companies</h5>
           <div className="options-container">
-            <button className="option-item active">Fiverr</button>
-            <button className="option-item">Natural Intellegence</button>
+            <li className="option-item active">Fiverr</li>
+            <li className="option-item">Natural Intellegence</li>
           </div>
         </div>
         <div className="category-container">
           <h5 className="options-header">Departments</h5>
           <div className="options-container">
-            <button className="option-item active">Analytics</button>
-            <button className="option-item">Business</button>
-            <button className="option-item">
-              Customer Care and\nTrust & Safety
-            </button>
-            <button className="option-item">Data</button>
-            <button className="option-item">Design</button>
-            <button className="option-item">Finance</button>
-            <button className="option-item">Marketing</button>
-            <button className="option-item">Other</button>
-            <button className="option-item">Product</button>
-            <button className="option-item">Technology</button>
+            <li className="option-item active">Analytics</li>
+            <li className="option-item">Business</li>
+            <li className="option-item">Customer Care and\nTrust & Safety</li>
+            <li className="option-item">Data</li>
+            <li className="option-item">Design</li>
+            <li className="option-item">Finance</li>
+            <li className="option-item">Marketing</li>
+            <li className="option-item">Other</li>
+            <li className="option-item">Product</li>
+            <li className="option-item">Technology</li>
           </div>
         </div>
         <div className="category-container">
           <h5 className="options-header">Locations</h5>
           <div className="options-container">
-            <button className="option-item active">Tel Aviv</button>
+            <li className="option-item active">Tel Aviv</li>
           </div>
         </div>
       </div>
